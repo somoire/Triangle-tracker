4b8ea8d36865e0f807f0b18004f7bdf579948402
Rodney Somoire <rayr30667@gmail.com>
2019-02-17 12:39:42 -0500

    include any additional information

--- js/scripts.js
+++ js/scripts.js
@@ -9,11 +9,17 @@ alert ("EQUILATERAL TRIANGLE");
 else if (x === y || y === z || x === z) {
 alert ("ISOSCELES TRIANGLE");
 }
+else if (x + y > z || y + z > x || x + z > y){
+alert ("SCALENE TRIANGLE");
+}
 else if (x + y <= z || y + z <= x || x + z <= y){
-alert ("TRIANGLE CANNOT BE FORMED");
+alert ("TRIANGLE CANNOT BE FORMED")
 }
-else {
-alert ("SCALENE")
-}
+else if(isNaN(a) || isNaN(b) || isNaN(c)) {
+     display.innerHTML = "<p class='display-text'>That is <span class='not'>NOT</span> a number</p>";
+ }
+ else {
+   display.innerHTML = "<p class='display-text'>That is <span class='not'>NOT</span> a triangle</p><p>The sum of any two sides should be <span class='fill'>GREATER</span> than the third side</p>"
+ }
 }
 identifyTriangle();
